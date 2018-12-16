710844f4fb9d7e3a2e1f35a3dadd4d155128cf1a
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-16 22:10:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var item = [{ `itemName: nameOfTheItem, itemPrice: priceOfTheItem` }]
+  var item = [{itemName: nameOfTheItem, itemPrice: priceOfTheItem}]
 }
 
 function viewCart() {
